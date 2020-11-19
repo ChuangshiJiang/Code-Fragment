fa66ec1f8e14d4f091fc73fb1f28b3575e5b24a3
unknown <jiangchuangshi@focuschina.com>
2020-11-19 16:17:00 +0800

    optimize

--- js/download.js
+++ js/download.js
@@ -1,17 +1,29 @@
+/**
+* 下载方法
+* @author Jiang_Chuangshi
+* @date 2020/11/19 16:12
+* @param {String} url 链接地址
+* @param {Boolean} isBlob 是否blob类型
+* @param {String} blobData 如果是blob类型，传递blob数据
+* @param {String} name 下载之后的文件名
+* @returns {Void}
+*/
 export default function Download (url, isBlob, blobData, name) {
   let link = document.createElement('a');
-  link.style.display = 'none';
   let blobUrl;
+  link.style.display = 'none';
   if (isBlob) {
     blobUrl = window.URL.createObjectURL(url);
     if (window.navigator.msSaveOrOpenBlob) {
       window.navigator.msSaveOrOpenBlob(blobData, name);// 自定义下载文件名（如exemple.txt）
     } else {
       link.href = blobUrl;
-      link.setAttribute('download', name);
     }
   } else {
     link.href = url;
+  }
+  if(name){
+    link.setAttribute('download', name);
   }
   document.body.appendChild(link);
   link.click();
